2d625393324d51e6ec1501e61efca29eae75b0b7
Omr AL-shikh <69796956+EngAL-shikh@users.noreply.github.com>
2021-01-10 20:13:47 +0300

    Update MainActivity.kt

--- app/src/main/java/com/amroz/ystore/MainActivity.kt
+++ app/src/main/java/com/amroz/ystore/MainActivity.kt
@@ -12,16 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val isFragmentContainerEmpty = savedInstanceState == null
-        if (isFragmentContainerEmpty) {
-            supportFragmentManager
-                .beginTransaction()
-
-              //  .add(R.id.fragmentContainer, ReportFragment.newInstance("report"))
-
-                .add(R.id.fragmentContainer, Catogrey_Fragment.newInstance("Category"))
-
-                .commit()
-        }
+    
     }
 }
