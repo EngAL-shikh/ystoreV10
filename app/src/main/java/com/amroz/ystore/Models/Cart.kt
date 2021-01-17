8c80ca1da12b7bb89cc2584bc20c1393b5be505f
ruqaiah.saif <ruqaiah.saif@gmail.com>
2021-01-17 15:11:23 +0300

    Ruqaiah"

--- app/src/main/java/com/amroz/ystore/Models/Cart.kt
+++
@@ -1,7 +0,0 @@
-package com.amroz.ystore.Models
-
-data class Cart (
-    val cart_id:Int?=0,
-    val user_id:Int=0,
-   val product_id:Int=0,
- val Quantity:Int=0)
--- app/src/main/java/com/amroz/ystore/YstoreViewModels.kt
+++ app/src/main/java/com/amroz/ystore/YstoreViewModels.kt
@@ -5,29 +5,11 @@ package com.amroz.ystore
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.amroz.ystore.Models.Cart
-import com.amroz.ystore.Models.Products
-import com.amroz.ystore.Models.Report
-import com.amroz.ystore.Models.Users
+
 
 
 open class YstoreViewModels: ViewModel() {
 
-    var liveDataCategory: LiveData<List<Category>>
-    var liveDataCart: LiveData<List<Cart>>
-    var LiveDataReport: LiveData<List<Report>>
-    var LiveDataUsers: LiveData<List<Users>>
-    var LiveDataProducts: LiveData<List<Products>>
-
-/// init
-    init {
-        liveDataCategory = Featchers().fetchCat()
-        liveDataCart = Featchers().fetchCart()
-        LiveDataReport = Featchers().fetchReport()
-        LiveDataUsers = Featchers().fetchUsers()
-        LiveDataProducts = Featchers().fetchProducts()
-
-    }
 
 
 
